b94709034684fc896e53c38c22741ae47ae26b96
NathanielCH <nathaniel981@gmail.com>
2020-11-27 00:00:37 -0600

    Now able to read csv files

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Hola")
+import csv
+from collections import namedtuple 
+
+Personas = namedtuple('Personas', 'name, age')
+
+
+for p in map(Personas._make, csv.reader(open("test.csv", "r"))):
+    print(p.name, p.age)
