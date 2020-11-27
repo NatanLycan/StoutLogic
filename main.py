490e959368a9ac7b26fec96e17917359b8bac1d9
NathanielCH <nathaniel981@gmail.com>
2020-11-27 11:57:07 -0600

    Se crea estructura del workspace

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import csv
 from collections import namedtuple 
 
 Personas = namedtuple('Personas', 'name, age')
+path="test.csv"
 
-
-for p in map(Personas._make, csv.reader(open("test.csv", "r"))):
+for p in map(Personas._make, csv.reader(open(path, "r"))):
     print(p.name, p.age)
