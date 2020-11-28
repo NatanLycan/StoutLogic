f9982267f4ddcd0278e6872bfb563cbef71f5b0d
NathanielCH <nathaniel981@gmail.com>
2020-11-27 19:17:18 -0600

    Anderson-Darling TEst Added

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from collections import namedtuple as nt
 import numpy as np
 from scipy import stats
 
+#VAriables de Ejecucion
 file = csv.reader(open("data/raw/data/ubicaciones.csv", "r",encoding='utf-8-sig'), delimiter=',')
 f = csv.reader(open("data/raw/data/ubicaciones.csv", "r",encoding='utf-8-sig'), delimiter=',')
 line_count = 0
@@ -15,6 +16,12 @@ minlat=0#y
 maxlat=0#y
 minlon=0#x
 maxlon=0#x
+vol_total=0
+
+
+##VAriables de respuesta final
+cliente = nt('cliente','CLIENTE, D1, D2, D3, D4, D5, D6')
+respuesta=[]
 
 #Obtengo las distintas frecuencias
 for r in f:
@@ -40,6 +47,7 @@ for row in file:
         #print(p1)
         l.append(p1)
         line_count += 1
+        vol_total += float(row[3])
         if maxlat<p1.lat:
             maxlat=p1.lat
         elif minlat>p1.lat:
@@ -52,9 +60,9 @@ for row in file:
         for n in frecs:
             txt="if p1.Frecuencia=="+n+":\n\tx"+n+".append(p1.lon)\n\ty"+n+".append(p1.lat)"
             exec(txt)
+print("Volumen disponible: "+str(vol_total)+"\nVolumen por Grupo: "+str(vol_total/6))
 
-
-#################Grafico Distribucion de entregas
+#################Grafico Distribucion de entregas**********************s
 #for n in frecs:
 #    txt="xe"+n+"=np.array(x"+n+")\nye"+n+"=np.array(y"+n+")\nplt.scatter(xe"+n+", ye"+n+",label='Frecuencia "+n+"')"
 #    exec(txt)
@@ -64,15 +72,20 @@ for row in file:
 
 
 
-#Probability Distribution Frequency PDF
-bins = np.linspace(min(map(int,frecs))-1, max(map(int,frecs))+1, 10)
-histogram, bins = np.histogram(tuple(map(int,todas_frecs)), bins=bins, density=True)
-bin_centers = 0.5*(bins[1:] + bins[:-1])
-print(bins,histogram,bin_centers)
+##########Probability Distribution Frequency PDF***************
+#bins = np.linspace(min(map(int,frecs))-1, max(map(int,frecs))+1, 10)
+#histogram, bins = np.histogram(tuple(map(int,todas_frecs)), bins=bins, density=True)
+#bin_centers = 0.5*(bins[1:] + bins[:-1])
+#print(bins,histogram,bin_centers)
 
-pdf = stats.norm.pdf(bin_centers)
+#pdf = stats.norm.pdf(bin_centers)
 
-plt.plot(bin_centers, histogram, label="Histogram of samples")
-plt.plot(bin_centers, pdf, label="PDF")
-plt.legend()
-plt.show()
+#plt.plot(bin_centers, histogram, label="Histogram of samples")
+#plt.plot(bin_centers, pdf, label="PDF")
+#plt.legend()
+#plt.show()
+
+##############################Anderson-DArling Test
+#from scipy.stats import anderson
+#a=anderson(tuple(map(int,todas_frecs)))
+#print(a)
